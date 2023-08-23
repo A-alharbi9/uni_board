1ebba323dc4f6798ea7ab372649698523312c58e
Abdullah <A21alharbi9@gmail.com>
2023-08-23 17:47:15 +0300

    feat: Add types to mongoose Connect function

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,17 @@
-import _mongoose, { connect } from 'mongoose';
+import _mongoose, { connect, ConnectOptions } from 'mongoose';
 
 declare global {
   var mongoose: {
-    promise: Promise<typeof connect | void> | null;
+    promise: Promise<typeof _mongoose | void> | null;
     conn: typeof _mongoose | null;
   };
 }
 
 let cached = global.mongoose;
-const { DB_URI } = process.env;
+
+const DB_URI: string = process.env.DB_URI as string;
+
+const options: ConnectOptions = { bufferCommands: false };
 
 if (!cached) {
   cached = global.mongoose = { promise: null, conn: null };
@@ -19,7 +22,7 @@ async function connectDb() {
     return cached.conn;
   }
   if (!cached.promise) {
-    cached.promise = connect(DB_URI, { bufferCommands: false })
+    cached.promise = connect(DB_URI, options)
       .then((connection) => {
         cached.conn = connection;
 
